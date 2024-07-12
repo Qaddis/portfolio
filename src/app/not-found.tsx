11329d93ece8e1741be77cfb2aa10f0a909a7cf4
Qaddis <svyatoslavb1107@gmail.com>
2024-07-12 17:00:51 +0300

    Not Found page has been added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { motion, type Transition } from "framer-motion"
+import { useRouter } from "next/navigation"
+
+export default function NotFoundPage() {
+	const router = useRouter()
+
+	const handleClick = (): void => {
+		router.replace("/")
+	}
+
+	const transition: Transition = {
+		duration: 2,
+		repeat: Infinity,
+		ease: "easeInOut"
+	}
+
+	return (
+		<section className="not-found">
+			<motion.h1
+				initial={{ scale: 1, color: "var(--spec)" }}
+				animate={{
+					scale: [1, 1.2, 1],
+					color: ["var(--spec)", "var(--violet)", "var(--spec)"]
+				}}
+				transition={transition}
+			>
+				Упс... Ошибка 404
+			</motion.h1>
+			<p>
+				Страница не найдена <b>┐(‘～` )┌</b>
+				<br />
+				Более того - тут всего одна <span onClick={handleClick}>страница</span>!
+			</p>
+
+			<button onClick={handleClick}>На главную!</button>
+		</section>
+	)
+}
