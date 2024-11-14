68c0c6665c1519b3b8c31f4a5f3e6b43409c6365
Qaddis <svyatoslavb1107@gmail.com>
2024-11-14 20:47:45 +0300

    The animation of transitions between pages has been adapted for mobile devices

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,15 +1,19 @@
 "use client"
 
-import { isTransitionAtom } from "@/store/store"
 import { motion } from "framer-motion"
 import { useAtom } from "jotai"
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
+
+import { isTransitionAtom } from "@/store/store"
 import styles from "./overlay.module.scss"
 
 export default function Overlay() {
 	const [isTransition, setTransition] = useAtom(isTransitionAtom)
 	const [isAnimate, setIsAnimate] = useState<boolean>(false)
+	const path = usePathname()
+
+	const blocks: string[] = ["w", "a", "i", "t"]
 
 	useEffect(() => {
 		if (isTransition) {
@@ -23,28 +27,42 @@ export default function Overlay() {
 		}
 	}, [isTransition])
 
-	const path = usePathname()
-
 	useEffect(() => {
 		setTransition(false)
 	}, [path])
 
-	const blocks: string[] = ["w", "a", "i", "t"]
+	const [isMobile, setIsMobile] = useState<boolean>(false)
+
+	useEffect(() => {
+		if (window.screen.width <= 768) setIsMobile(true)
+	})
 
 	return (
 		<motion.div
 			initial={false}
 			animate={isTransition ? "show" : "hide"}
 			style={{ display: isAnimate ? "grid" : "none" }}
-			className={styles.overlay}
+			className={
+				isMobile ? `${styles.overlay} ${styles.mobile}` : styles.overlay
+			}
 		>
 			{blocks.map((item, index) => (
 				<motion.div
 					key={index}
-					variants={{
-						hide: { y: "100%" },
-						show: { y: 0 }
-					}}
+					variants={
+						isMobile
+							? {
+									hide:
+										index % 2 === 0
+											? { x: "-100%", y: 0 }
+											: { x: "100%", y: 0 },
+									show: { x: 0, y: 0 }
+								}
+							: {
+									hide: { y: "100%", x: 0 },
+									show: { y: 0, x: 0 }
+								}
+					}
 					transition={{ duration: 0.25, delay: index / 10, ease: "easeInOut" }}
 					className={styles.block}
 				>
